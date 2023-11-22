981bb1a1610a700a91b6002f68852b66954c0fae
ali-bouali <bouali.pro@gmail.com>
2023-11-22 20:32:43 +0100

    init kafka demo project

---
+++ kafka-demo/src/test/java/com/alibou/kafka/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
