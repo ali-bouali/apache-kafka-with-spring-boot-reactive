9ca23bededd55f1a58f31b0af5d51ca1f76f342c
ali-bouali <bouali.pro@gmail.com>
2023-11-23 19:47:43 +0100

    add kafka producers

---
+++ kafka-demo/src/main/java/com/alibou/kafka/producer/KafkaJsonProducer.java
@@ -0,0 +1,26 @@
+package com.alibou.kafka.producer;
+
+import com.alibou.kafka.payload.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaJsonProducer {
+
+    private final KafkaTemplate<String, Student> kafkaTemplate;
+
+    public void sendMessage(Student student) {
+
+        Message<Student> message = MessageBuilder
+                .withPayload(student)
+                .setHeader(KafkaHeaders.TOPIC, "alibou")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
