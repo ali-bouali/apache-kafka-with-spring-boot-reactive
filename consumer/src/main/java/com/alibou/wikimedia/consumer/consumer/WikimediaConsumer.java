f979a84b1aca9c1b231162c9f4bf21c410e506a8
ali-bouali <bouali.pro@gmail.com>
2023-11-23 19:48:36 +0100

    implement consumer project

---
+++ consumer/src/main/java/com/alibou/wikimedia/consumer/consumer/WikimediaConsumer.java
@@ -0,0 +1,18 @@
+package com.alibou.wikimedia.consumer.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@Slf4j
+public class WikimediaConsumer {
+
+    @KafkaListener(topics = "wikimedia-stream", groupId = "myGroup")
+    public void consumeMsg(String msg) {
+        log.info(format("Consuming the message from wikimedia-stream Topic:: %s", msg));
+        // Please feel free to do anything you want with the data
+    }
+}
---
+++ consumer/src/test/java/com/alibou/wikimedia/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.wikimedia.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
